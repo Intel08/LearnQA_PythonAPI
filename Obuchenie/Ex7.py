5f35e662be4da8847542d1a7f919a498eef516e0
intel90 <tsakirovm@gmail.com>
2024-07-18 15:58:55 +0300

    перенос

--- Obuchenie/Ex7.py
+++ Obuchenie/Ex7.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
-print(response.status_code)
+print(response.text)
 
 payload = {"method": "GET"}
 response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload)
